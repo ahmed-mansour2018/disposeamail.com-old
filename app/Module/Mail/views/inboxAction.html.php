f7da3c6cb6df4a5192d36455333a044578d51b3d
Vance Lucas <vance@vancelucas.com>
2011-02-02 15:39:04 -0600

    Mail inbox updates and style updates

--- app/Module/Mail/views/inboxAction.html.php
+++ app/Module/Mail/views/inboxAction.html.php
@@ -1,11 +1,15 @@
+<?php
+$view->title('INBOX for ' . $username . '@disposeamail.com');
+?>
 <h2>INBOX: <?php echo $username; ?>@disposeamail.com</h2>
 
+<div id="mail_inbox">
 <?php
 // Using 'datagrid' Generic
-$kernel = $this->kernel;
-$grid = $this->generic('datagrid')
+$kernel = $view->kernel;
+$grid = $view->generic('datagrid')
 	->data($mail)
-	->column('Subject', function($view, $item) use($kernel, $username) {
+	->column('Subject', function($item) use($view, $kernel, $username) {
 		// Build URL from route
 		$msgUrl = $view->url(array(
 			'module' => 'Mail',
@@ -17,13 +21,18 @@ $grid = $this->generic('datagrid')
 		// Reutrn HTML string
 		return "<a href='" . $msgUrl . "' title='" . $item->subject . "'>" . $kernel->truncate($item->subject, 40) . "</a>";
 	})
-	->column('From', function($view, $item) use($kernel) {
+	->column('From', function($item) use($kernel) {
 		return "<span title='" . $item->from . "'>" . $kernel->truncate($item->from) . "</span>";
 	})
-	->column('Date', function($view, $item) {
+	->column('Date', function($item) use($view) {
 		return "<nobr>" . $view->toDate($item->date_message) . "</nobr>";
+	})
+	// Called if dataset is empty
+	->noData(function() use($username) {
+		return '<p>No mail found for ' . $username . '</p>';
 	});
 
 // Rendering is a simple 'echo', just like View templates
 echo $grid;
 ?>
+</div>
