a911e1ab2f661435bb4ff0019e471b381642fc8d
Vance Lucas <vance@vancelucas.com>
2011-02-02 12:12:28 -0600

    Config: Added override config with correct DB info

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,14 +2,14 @@
 /**
 * Default routes
 */
+$router->route('default', '/')
+    ->defaults(array('module' => 'Page', 'action' => 'view', 'page' => 'home', 'format' => 'html'));
+    
 $router->route('page_faq', '/faq')
     ->defaults(array('module' => 'Page', 'action' => 'view', 'page' => 'faq', 'format' => 'html'));
 
+$router->route('inbox', '/inbox/<:username>(.<:format>)') // :format optional
+    ->defaults(array('module' => 'Mail', 'action' => 'inbox', 'format' => 'html'));
+
 $router->route('module_action', '/<:module>/<:action>(.<:format>)') // :format optional
     ->defaults(array('format' => 'html'));
-
-$router->route('inbox', '/inbox/<:username>(.<:format>)') // :format optional
-    ->defaults(array('module' => 'Mail', 'action' => 'index', 'format' => 'html'));
-
-$router->route('default', '/')
-    ->defaults(array('module' => 'Page', 'action' => 'view', 'page' => 'home', 'format' => 'html'));
